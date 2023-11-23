e2537a46ee459a8e4bd5e11907d5b8af26d82b11
Harry Sim <harry5272@naver.com>
2023-11-23 22:50:16 +0900

    socket.io를 이용한 채팅소캣 연결

--- src/init.js
+++ src/init.js
@@ -1,8 +1,17 @@
 import app from "./app";
+import http from "http";
+import {Server} from "socket.io";
 
 const PORT = 4885;
 
 const handleListening = () => 
     console.log(`✅ 서버연결 성공!: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const httpServer = http.createServer(app);
+const ioServer = new Server(httpServer);
+
+ioServer.on("connection", (socket)=>{
+    console.log(socket);
+});
+
+httpServer.listen(PORT, handleListening);
